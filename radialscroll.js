45a0a36b03d04ac1640d9fd7f5b28b2c261099df
Clark McLean <clarkmclean@gmail.com>
2015-02-06 20:21:09 -0700

    example rotator to show position in demo

--- radialscroll.js
+++ radialscroll.js
@@ -1,55 +1,62 @@
 $(function(){
+
+var clone = $('.circleContainer').clone();
+$('#modelContainer').append(clone);
+
 var s = skrollr.init([
     forceHeight=true
 ]);
 // for this example we are using p elements as blocks, we will probably have them assign a class
-var $block = $('p');
+$('.circleContainer').each(function(){
+    var $block = $(this).find('p');
 
-var rotation = 0;
-var location = 0
-var circumference = 0;
-var radius;
-var maxheight = 0;
-var pageheight = 0;
-var pagewidth = 0;
-var blockCount = 0;
-var angle = 0;
+    var rotation = 0;
+    var location = 0
+    var circumference = 0;
+    var radius;
+    var maxheight = 0;
+    var pageheight = 0;
+    var pagewidth = 0;
+    var blockCount = 0;
+    var angle = 0;
 
-var blockWidth = $block.width();
-pageheight = $(window).height();
-pagewidth = $(window).width();
-//pageheight = 100;
+    var blockWidth = $block.width();
+    pageheight = $(window).height();
+    pagewidth = $(window).width();
+    //pageheight = 100;
 
-$block.each(function () {
-    var height = $(this).height();
-    circumference += pageheight;
-    if (height > maxheight) {
-        maxheight = height;
+    $block.each(function () {
+        var height = $(this).height();
+        circumference += pageheight;
+        if (height > maxheight) {
+            maxheight = height;
+        }
+        $(this).css({
+            'margin-top' : (pageheight - height)/2 + 'px',
+            'margin-bottom' : (pageheight - height)/2 + 'px'
+        });
+        blockCount++;
+    });
+
+    // first find a radius for the circumference
+    radius = circumference / (2 * 3.14159);
+
+    $(this).find('.originContainer').css({
+        'left' : (-Math.abs(radius)+pagewidth/2-$block.width()/2) + 'px',
+        'width' : blockWidth + 'px',
+        'height' : pageheight + 'px'
+    });
+    angle = 360/blockCount;
+    var rotate = 0;
+    console.log('angle' + angle);
+    for (i = 0; i < blockCount; i++) {
+        console.log('here');
+        $(this).find('p:eq(' + i + ')').css({
+            'top' : radius * Math.sin(2 * Math.PI * i / blockCount),
+            'left' : radius * Math.cos(2 * Math.PI * i / blockCount),
+            'transform' : 'rotate(' + rotate + 'deg)'
+        });
+        rotate = rotate+angle;
     }
-    $(this).css({
-        'margin-top' : (pageheight - height)/2 + 'px',
-        'margin-bottom' : (pageheight - height)/2 + 'px'
-    });
-    blockCount++;
 });
-
-// first find a radius for the circumference
-radius = circumference / (2 * 3.14159);
-
-$('#originContainer').css({
-    'left' : (-Math.abs(radius)+pagewidth/2-$block.width()/2) + 'px',
-    'width' : blockWidth + 'px',
-    'height' : pageheight + 'px'
-});
-angle = 360/blockCount;
-var rotate = 0;
-console.log('angle' + angle);
-for (i = 0; i < blockCount; i++) {
-    $('p:eq(' + i + ')').css({
-        'top' : radius * Math.sin(2 * Math.PI * i / blockCount),
-        'left' : radius * Math.cos(2 * Math.PI * i / blockCount),
-        'transform' : 'rotate(' + rotate + 'deg)'
-    });
-    rotate = rotate+angle;
-}
 });
